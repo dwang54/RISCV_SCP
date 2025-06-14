a7f18059b1c59a5b6b54a4c805e67bcc357e4f2a
Daniel Wang <wang6161@purdue.edu>
2025-06-14 13:59:08 -0500

    pcmux

---
+++ src/pc_mux.sv
@@ -0,0 +1,11 @@
+module PC_mux (
+    input logic [31:0] PC_plus_4, PC_target, 
+    input logic PC_src,
+    output logic [32:0] PC_next
+);
+
+    assign PC_next = (PC_src) ? PC_target : PC_plus_4;
+
+
+
+endmodule
