6861d5980bb6a433217e0f6b8b3e3cd926e6c822
Daniel Wang <wang6161@purdue.edu>
2025-06-14 11:31:30 -0500

    pc file

---
+++ src/pc.sv
@@ -0,0 +1,19 @@
+module PC (
+    input logic clk, rst,
+    input logic [31:0] PC_next,
+    output logic [31:0] PC_out
+);
+
+    logic [31:0] PC_reg;
+
+    always_ff @(posedge clk or posedge reset) begin
+        if (rst) 
+            PC_reg <= 0;
+        else 
+            PC_reg <= PC_next;
+    end
+
+    assign PC_out = PC_reg;
+
+
+endmodule
