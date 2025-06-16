4d99a026687b80131fe6836316c3d24c17b6b8ef
Daniel Wang <wang6161@purdue.edu>
2025-06-16 09:41:44 -0500

    pc mux updates

--- src/pc_mux.sv
+++ src/pc_mux.sv
@@ -1,9 +1,9 @@
 module PC_mux (
     input logic [31:0] PC_plus_4, PC_target, 
-    input logic PC_src,
+    input logic PCSrc,
     output logic [32:0] PC_next
 );
 
-    assign PC_next = (PC_src) ? PC_target : PC_plus_4;
+    assign PC_next = (PCSrc) ? PC_target : PC_plus_4;
 
 endmodule
