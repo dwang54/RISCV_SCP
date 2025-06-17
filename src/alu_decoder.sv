5e31289112c43c30971310b1b0f285f5f3d3ae5f
Daniel Wang <wang6161@purdue.edu>
2025-06-17 12:38:12 -0500

    alu decoder

--- src/alu_decoder.sv
+++ src/alu_decoder.sv
@@ -10,33 +10,33 @@ module alu_decoder (
     always_comb begin
         case (ALUOp):
             2'b00:
-                ALUControl = 4'b0010;               // addition
+                ALUControl = 4'b0010;                   // addition
             2'b01:
-                ALUControl = 4'b0110;               // subtraction
+                ALUControl = 4'b0110;                   // subtraction
             2'b10:
                 case (funct3):
                     3'b000:
-                        if (funct7b5 & opb5)        // R-type subtract
+                        if (funct7b5 & opb5)            // R-type 
                             ALUControl = 4'b0110;
                         else 
                             ALUControl = 4'b0010;
                     3'b001:
-                        ALUControl = 4'b1010;       // sll, slli
+                        ALUControl = 4'b1010;           // sll, slli
                     3'b010:
-                        ALUControl = 4'b0111;       // slt
+                        ALUControl = 4'b0111;           // slt
                     3'b011:
-                        ALUControl = 4'b1000;        // sltu
+                        ALUControl = 4'b1000;           // sltu
                     3'b100:
-                        ALUControl = 4'b0100;        // xor
+                        ALUControl = 4'b0100;           // xor
                     3'b101:
                         if (funct7b5)
-                            ALUControl = 4'b1100;   // sra
+                            ALUControl = 4'b1100;       // sra
                         else 
-                            ALUControl = 4'b1011;   // srl
+                            ALUControl = 4'b1011;       // srl
                     3'b110:
-                        ALUControl = 4'b0001;       // or
+                        ALUControl = 4'b0001;           // or
                     3'b111:
-                        ALUControl = 4'b0000;          // and
+                        ALUControl = 4'b0000;           // and
                     default:
                         ALUControl = 4'b1111;
                 endcase
