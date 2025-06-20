633a23ed41483bc9e853e32ad050b419414e6bd6
Daniel Wang <wang6161@purdue.edu>
2025-06-20 09:36:52 -0500

    top module

--- rtl/top.sv
+++ rtl/top.sv
@@ -1,6 +1,34 @@
 module top (
-
+    input logic clk, rst,
+    input logic [31:0] WriteData, DataAddr,
+    output logic MemWrite
 );
+
+    logic [31:0] PC, Instr, ReadData;
+
+    Instruction_Memory instruction_memory (
+        .A(PC),
+        .RD(Instr)
+    );
+
+    Data_Memory data_memory (
+        .clk(clk),
+        .WriteEnable(MemWrite),
+        .ALUResult(DataAddr),
+        .WriteData(WriteData),
+        .ReadData(ReadData)
+    );
+
+    SS_Core ss_core (
+        .clk(clk),
+        .rst(rst),
+        .Instr(Instr),
+        .ReadData(ReadData),
+        .PC(PC),
+        .MemWrite(MemWrite),
+        .ALUResult(DataAddr),
+        .WriteData(WriteData)
+    );
 
 
 
