b713ac1548a8f67f5d861876aa4c905dec919663
Daniel Wang <wang6161@purdue.edu>
2025-06-20 09:30:31 -0500

    deleted files

--- src/pc_mux.sv
+++
@@ -1,9 +0,0 @@
-module PC_mux (
-    input logic [31:0] PC_plus_4, PC_target, 
-    input logic PCSrc,
-    output logic [32:0] PC_next
-);
-
-    assign PC_next = (PCSrc) ? PC_target : PC_plus_4;
-
-endmodule
--- src/pc_plus_4.sv
+++
@@ -1,8 +0,0 @@
-module PC_plus_4 (
-    input logic [31:0] PC,
-    output logic [31:0] PC_plus_4
-);
-
-    assign PC_plus_4 = PC + 32'd4;
-
-endmodule
