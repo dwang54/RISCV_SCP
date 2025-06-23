5f2b5c5d42fdc4a763d8d59808acbcf7998b307b
Daniel Wang <wang6161@purdue.edu>
2025-06-23 16:43:24 -0500

    alu mux tb

---
+++ tb/alu_mux_tb.sv
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+
+module Alu_Mux_tb;
+
+logic [31:0] RD2;
+logic [31:0] ImmExt;
+logic ALUSrc;
+logic [31:0] SrcB;
+
+Alu_Mux dut (
+    .RD2(RD2),
+    .ImmExt(ImmExt),
+    .ALUSrc(ALUSrc),
+    .SrcB(SrcB)
+);
+
+initial begin
+    $dumpfile("Alu_Mux.vcd");
+    $dumpvars(0, Alu_Mux_tb);
+
+    // case 1
+    RD2 = 32'hAAAAAAAA;
+    ImmExt = 32'h12345678;
+    ALUSrc = 0;
+    #10
+    $display("ALUSrc=%b, SrcB=%h (Expected: %h)", ALUSrc, SrcB, RD2);
+
+    // case 2
+    ALUSrc = 1;
+    #10;
+    $display("ALUSrc=%b, SrcB=%h (Expected: %h)", ALUSrc, SrcB, ImmExt);
+
+
+    // case 3
+    RD2 = 32'hDEADBEEF;
+    ImmExt = 32'hBEEFDEAD;
+    ALUSrc = 0;
+    #10;
+    $display("ALUSrc=%b, SrcB=%h (Expected: %h)", ALUSrc, SrcB, RD2);
+
+
+end
+
+endmodule
