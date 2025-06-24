c58bc3d7eab8d3c542630849dba80bc328ef95fa
Daniel Wang <wang6161@purdue.edu>
2025-06-23 19:19:22 -0500

    rtl final

--- rtl/pc_mux.sv
+++ rtl/pc_mux.sv
@@ -3,7 +3,7 @@
 module PC_mux (
     input logic [31:0] PCplus4, PC_target, 
     input logic PCSrc,
-    output logic [32:0] PC_next
+    output logic [31:0] PC_next
 );
 
     assign PC_next = (PCSrc) ? PC_target : PC_plus_4;
